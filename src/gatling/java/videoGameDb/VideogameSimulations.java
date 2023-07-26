0f803874b4a2961e294695b35c0381b01b503ec4
Vinayak Patil <vinayakpatil1524@gmail.com>
2023-07-26 20:26:59 +0530

    First commit

---
+++ src/gatling/java/videoGameDb/VideogameSimulations.java
@@ -0,0 +1,60 @@
+package videoGameDb;
+
+import java.time.Duration;
+import java.util.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class VideogameSimulations extends Simulation {
+
+  private HttpProtocolBuilder httpProtocol = http
+    .baseUrl("https://videogamedb.uk:443")
+    .inferHtmlResources()
+    .acceptHeader("application/json")
+    .acceptEncodingHeader("gzip, deflate, br")
+    .acceptLanguageHeader("en-GB,en;q=0.8")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
+
+    private List<ScenarioBuilder> scenarios = new ArrayList<>();
+  private ScenarioBuilder scn = scenario("Scenario to get all the video games")
+    .exec(
+      http("Scenario to get all the video games")
+        .get("/api/videogame")
+    ).pause(10);
+
+    private ScenarioBuilder scn2 = scenario("Scenario to get 1st video game")
+            .exec(
+                    http("Scenario to get 1st video game")
+                            .get("/api/videogame/1")
+            ).pause(4);
+
+    private ScenarioBuilder scn3 = scenario("Scenario to get 2nd video game")
+            .exec(
+                    http("Scenario to get 2nd video game")
+                            .get("/api/videogame/2")
+            ).pause(5);
+
+
+
+  {
+      scenarios.add(scn);
+    scenarios.add(scn2);
+      scenarios.add(scn3);
+
+   List<PopulationBuilder> pbs=new ArrayList<>();
+    for(ScenarioBuilder scenario:scenarios){
+   PopulationBuilder pbb=scenario.injectOpen(rampUsers(23).during(12)).protocols(httpProtocol);
+   pbs.add(pbb);
+    }
+
+    PopulationBuilder chaindedPopulationBuilder=pbs.stream().reduce(PopulationBuilder::andThen).orElseThrow(()->new IllegalStateException("No Pb Found"));
+
+    setUp(chaindedPopulationBuilder);
+
+  }
+
+}
